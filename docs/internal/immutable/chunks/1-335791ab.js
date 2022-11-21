c50aadbd12ae9e791d237e7719c1e93f8623c3ef
pjsr1980 <pjsr1980@gmail.com>
2022-11-21 14:54:56 +0000

    added docs

---
+++ docs/internal/immutable/chunks/1-335791ab.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-1918051c.js";export{t as component};
---
+++ docs/internal/immutable/chunks/2-aa898ff3.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-738eb73d.js";export{t as component};
